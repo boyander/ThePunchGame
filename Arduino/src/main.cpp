3fea743e9dee5477e369bece286b28187b569270
Marc Pomar Torres <boyander@gmail.com>
2012-05-28 15:35:05 +0200

    modified:   Arduino/src/main.cpp

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -15,15 +15,9 @@ ACC acc(0,1,2);
 void setup(){
 
 	Serial.begin(115200);
-<<<<<<< HEAD
 	acc.calibrate();
 	//Blocks until new conection request
 	/*if(wp.connect(true)){
-=======
-
-	//Blocks until new conection request
-	if(wp.connect(true)){
->>>>>>> 6625c833fe699a877946e3acdd61a5268d8e78d3
 		//Blink led showing conection ok
 		pinMode(led, OUTPUT);
 		digitalWrite(led,HIGH);
@@ -33,13 +27,7 @@ void setup(){
 }
 
 void loop(){
-<<<<<<< HEAD
 	//Serial.println("Connected with -> " + wp.client);
 	acc.printACCForces();
-=======
-	Serial.println("Connected with -> " + wp.client);
-	acc.readData();
-	acc.printData();
->>>>>>> 6625c833fe699a877946e3acdd61a5268d8e78d3
 	delay(100);
 }
