76b10993a565da659275fa9d30acb1709eef0152
Marc Pomar Torres <boyander@gmail.com>
2012-06-03 22:59:07 +0200

    changes

--- NodeServer/server.js
+++ NodeServer/server.js
@@ -99,6 +99,7 @@ io.sockets.on('connection', function (socket) {
 	/* Welcome to new users, so let's send data to client */
 	socket.on('start-game', function (data) {
 		resetCalculus();
+		gameOn = true;
 		io.sockets.emit('game-status',{'gameON':true,'game-reset':true});
 	});
 
