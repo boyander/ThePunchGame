36214fd706e2eb068e49bdd078c30ccf86914bed
Marc Pomar Torres <boyander@gmail.com>
2012-06-03 15:29:38 +0200

    changes

--- PythonServer/static/js/punchgame.js
+++ PythonServer/static/js/punchgame.js
@@ -108,13 +108,3 @@ $(document).ready(function(){
 		};
 	});
 });
-
-// Load the SDK Asynchronously
-(function(d){
- var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
- if (d.getElementById(id)) {return;}
- js = d.createElement('script'); js.id = id; js.async = true;
- js.src = "//connect.facebook.net/en_US/all.js";
- ref.parentNode.insertBefore(js, ref);
-}(document));
-
